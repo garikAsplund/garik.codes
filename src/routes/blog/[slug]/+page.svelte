ffe41c422ab8a547cfab37bf22046b341fc3a4fd
Garik Asplund <garik.asplund@gmail.com>
2023-08-13 20:45:01 -0700

    text justify in blog, can't resist

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -7,7 +7,7 @@
 <article class="container h-full mx-auto flex flex-col items-center">
   <h1 class=" h-28 font-extrabold text-transparent text-4xl md:text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600
   m-10">{title}</h1>
-  <div class="w-5/6 prose dark:prose-invert">
+  <div class="w-5/6 prose dark:prose-invert text-justify">
     <Content />
   </div>
   <br>
