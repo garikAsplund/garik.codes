41eb1bdd14aea8f2cff6eb95597380a8e2a1f7da
Garik Asplund <garik.asplund@gmail.com>
2023-08-14 18:28:17 -0700

    better readability

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,12 @@
 	<div class="space-y-5 w-5/6">
 		<h1 class="h1"><span class="gradient-heading text-6xl">Garik Asplund</span></h1>
 		<h2 class="text-2xl">Full stack software engineer</h2>
-		<ul>
-			<li><code class="code">enjoys the simplicity of SvelteKit</code> - minimum code for maximum effect</li>
-			<li><code class="code">has built games with Unity</code> - new systems are fun to learn</li>
-			<li><code class="code">prefers to code in Rust</code> - speed, safety, and stability!?</li>
+		<ul class="lg:text-lg m-5 md:space-y-1 lg:space-y-2">
+			<li><code class="code lg:text-lg ">enjoys the simplicity of SvelteKit</code> - minimum code for maximum effect</li>
+			<li><code class="code lg:text-lg">has built games with Unity</code> - new systems are fun to learn</li>
+			<li><code class="code lg:text-lg">prefers to code in Rust</code> - speed, safety, and stability!?</li>
 		</ul>
-		<p>
+		<p class="lg:text-lg">
 			Currently building experiments for researchers at Yale-NUS that focus on attentional blink and how humans focus.
 		</p>
 	</div>
